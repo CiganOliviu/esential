4db0d433da0514fa58911fc48935dce584dd2e29
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-02-06 21:22:36 +0200

    update

--- dependencies/esentialDef.hpp
+++ dependencies/esentialDef.hpp
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "../easyPass/easyPass.hpp"
+#include "../tests/easyPass.hpp"
 #include "errorsHandler.hpp"
 
 class oneDimensionalArraysWorkFlow {
