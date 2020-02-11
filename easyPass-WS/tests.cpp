b627ac1f2e8439caae7a9c722e82646ae757eaa0
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-02-11 21:00:29 +0200

    update

---
+++ easyPass-WS/tests.cpp
@@ -0,0 +1,43 @@
+#include "easyPass.hpp"
+
+// development note: minim -100 000
+
+class setParametersWorkflow {
+
+public:
+	setParametersWorkflow ();
+
+	virtual ~setParametersWorkflow ();
+};
+
+class oneDimensionalArrayTestingWorkflow {
+
+public:
+	oneDimensionalArrayTestingWorkflow ();
+
+	virtual ~oneDimensionalArrayTestingWorkflow ();
+};
+
+class matricesTestingWorkFlow {
+
+public:
+	matricesTestingWorkFlow ();
+
+	virtual ~matricesTestingWorkFlow ();
+};
+
+class fundamentalAlgorithmsTestingWorkflow {
+
+public:
+	fundamentalAlgorithmsTestingWorkflow ();
+
+	virtual ~fundamentalAlgorithmsTestingWorkflow ();
+};
+
+int main(int argc, char const *argv[]) {
+
+	assertions __assert__;
+	validationRules __validations;
+
+	return 0;
+}
