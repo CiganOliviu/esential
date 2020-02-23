d000cc206c31974cfea1695464fda2692ec1ab24
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-02-23 21:32:58 +0200

    update

---
+++ samples/getMultipleWriteOnce/getMultipleWriteOnce.cpp
@@ -0,0 +1,42 @@
+/*
+  Tasks:
+    * read one dimensional array from file
+    * print one dimensional array
+    * print every value once
+*/
+
+#include "../../esential.hpp"
+
+class getMultipleWriteOnce {
+private:
+  errorsHandler __handler__;
+
+public:
+  getMultipleWriteOnce () {}
+
+  template <class Type> void writeValuesFromODAOnce (oneDimensionalArrayType<Type> ODAObject);
+
+  virtual ~getMultipleWriteOnce () {}
+};
+
+template <class Type> void getMultipleWriteOnce::writeValuesFromODAOnce (oneDimensionalArrayType<Type> ODAObject) {
+
+  __handler__.standardHandlerOneDimensionalArray (ODAObject, __PRETTY_FUNCTION__);
+
+  for (size_t iterator = ODAObject.startPoint; iterator < ODAObject.length + ODAObject.endPoint; iterator++)
+    if (ODAObject.oneDimensionalArray[iterator] != ODAObject.oneDimensionalArray[iterator + 1])
+      std::cout << ODAObject.oneDimensionalArray[iterator] << " ";
+}
+
+int main(int argc, char const *argv[]) {
+
+  oneDimensionalArrayType<int> ODAObject;
+  oneDimensionalArraysWorkFlow ODAWorkFlow;
+  getMultipleWriteOnce getMultipleWriteOnceWorkFlow;
+
+  ODAWorkFlow.readDynamicFileOneDimensionalArray ((char*)"getMultipleWriteOnceData.data", ODAObject);
+  ODAWorkFlow.outputOneDimensionalArray (ODAObject);
+  getMultipleWriteOnceWorkFlow.writeValuesFromODAOnce (ODAObject);
+
+  return 0;
+}
