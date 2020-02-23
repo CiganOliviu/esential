ae141d0bb73aaf5780aa01ee87e9178d96128166
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-02-23 21:35:18 +0200

    update

---
+++ samples/multipleOfThree/multipleOfThree.cpp
@@ -0,0 +1,60 @@
+/*
+  Tasks:
+    * read from file one dimensional array;
+    * print one dimensional array
+    * get the three minimum values who are multiple with 3
+*/
+
+#include "../../esential.hpp"
+
+class multipleOfThree {
+private:
+  errorsHandler __handler__;
+  validationRules __validations__;
+
+  bool isMultipleOfThree (int number);
+
+public:
+  multipleOfThree () {}
+
+  template <class Type> void getMultipleOfThreeNumbersFromODA (oneDimensionalArrayType<Type> ODAObject);
+
+  virtual ~multipleOfThree () {}
+};
+
+bool multipleOfThree::isMultipleOfThree (int number) {
+
+  if (__validations__.isNegativeOrZero (number)) return false; 
+
+  if (number % 3 == 0) return true;
+
+  return false;
+}
+
+template <class Type> void multipleOfThree::getMultipleOfThreeNumbersFromODA (oneDimensionalArrayType<Type> ODAObject) {
+
+  __handler__.standardHandlerOneDimensionalArray (ODAObject, __PRETTY_FUNCTION__);
+
+  int contor = 0;
+
+  for (size_t iterator = ODAObject.startPoint; iterator < ODAObject.length + ODAObject.endPoint; iterator++)
+    if (isMultipleOfThree(ODAObject.oneDimensionalArray[iterator]) && contor < 3) {
+      std::cout << ODAObject.oneDimensionalArray[iterator] << " ";
+      contor += 1;
+    }
+}
+
+int main(int argc, char const *argv[]) {
+
+  oneDimensionalArrayType<int> ODARefference;
+  oneDimensionalArraysWorkFlow ODAWorkFlow;
+  multipleOfThree multipleOfThreeRefference;
+
+  ODAWorkFlow.readDynamicFileOneDimensionalArray ((char*)"multipleOfThreeData.data", ODARefference);
+  ODAWorkFlow.outputOneDimensionalArray (ODARefference);
+  ODAWorkFlow.shellSort (ODARefference);
+  ODAWorkFlow.outputOneDimensionalArray (ODARefference);
+  multipleOfThreeRefference.getMultipleOfThreeNumbersFromODA (ODARefference);
+
+  return 0;
+}
