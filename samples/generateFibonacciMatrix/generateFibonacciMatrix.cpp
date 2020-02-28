6f45e39ec0be14ccfc429bd711ac1774b27e6bd0
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-02-28 20:53:39 +0200

    create generateFibonacciMatrix.cpp

---
+++ samples/generateFibonacciMatrix/generateFibonacciMatrix.cpp
@@ -0,0 +1,65 @@
+/*
+  tasks:
+    * generate a fibonacci number as in the example
+    0   1   1   2
+    13  8   5   3
+    21  34  55  89
+    610 377 233 144
+    * output the matrix
+*/
+
+#include "../../esential.hpp"
+
+class matrixGenerator {
+private:
+  errorsHandler __handler__;
+  fundamentalAlgorithmsWorkFlow __fundamental_algo__;
+
+public:
+  matrixGenerator () {}
+
+  void generateFibonacciMatrix (matrixType<int> & fiboMatrix);
+
+  virtual ~matrixGenerator () {}
+};
+
+void matrixGenerator::generateFibonacciMatrix (matrixType<int> & fiboMatrix) {
+
+  fiboMatrix.columnRefference = fiboMatrix.lineRefference;
+
+  __handler__.standardHandlerMatrix (fiboMatrix, __PRETTY_FUNCTION__);
+
+  int fiboIndexOdd = 1;
+  int fiboIndexEven = 0;
+
+  for (size_t iterator = fiboMatrix.startLinePoint; iterator < fiboMatrix.lineRefference + fiboMatrix.endLinePoint; iterator++) {
+      if (__fundamental_algo__.isOdd(iterator + 1)) {
+        fiboIndexOdd += fiboIndexEven;
+        for (size_t jiterator = fiboMatrix.startColumnPoint; jiterator < fiboMatrix.columnRefference + fiboMatrix.endColumnPoint; jiterator++) {
+          fiboMatrix.matrix[iterator][jiterator] = __fundamental_algo__.getThe_N_fibonacciNumber(fiboIndexOdd);
+          fiboIndexOdd += 1;
+        }
+      } else if (__fundamental_algo__.isEven(iterator + 1)) {
+          fiboIndexEven = fiboIndexOdd + fiboMatrix.lineRefference;
+          for (size_t jiterator = fiboMatrix.startColumnPoint; jiterator < fiboMatrix.columnRefference + fiboMatrix.endColumnPoint; jiterator++) {
+            fiboMatrix.matrix[iterator][jiterator] = __fundamental_algo__.getThe_N_fibonacciNumber(fiboIndexEven);
+            fiboIndexEven -= 1;
+          }
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+
+  matricesWorkFlow MWorkFlow;
+  matrixType <int> matrixRefference;
+  matrixGenerator __generator_init__;
+
+  std::cin >> matrixRefference.lineRefference;
+
+  __generator_init__.generateFibonacciMatrix(matrixRefference);
+
+  MWorkFlow.outputMatrix(matrixRefference);
+
+  return 0;
+}
