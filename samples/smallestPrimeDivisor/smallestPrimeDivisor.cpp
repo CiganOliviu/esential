37aa687c60d5be74e287fe13b18d7cefed6e2a7b
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-03-01 19:50:42 +0200

    create smallestPrimeDivisor.cpp

---
+++ samples/smallestPrimeDivisor/smallestPrimeDivisor.cpp
@@ -0,0 +1,37 @@
+#include "../../esential.hpp"
+
+class smallestPrimeDivisorProcessor {
+private:
+  errorsHandler __handler__;
+  fundamentalAlgorithmsWorkFlow __fundamental_algo__;
+
+public:
+  smallestPrimeDivisorProcessor () {}
+
+  int getTheSmallestPrimeDivisor (int number);
+
+  virtual ~smallestPrimeDivisorProcessor () {}
+};
+
+int smallestPrimeDivisorProcessor::getTheSmallestPrimeDivisor (int number) {
+
+  __handler__.negativeNumberHandler (number, __PRETTY_FUNCTION__);
+
+  for (size_t iterator = 2; iterator <= number; iterator++)
+    if (number % iterator == 0 && __fundamental_algo__.isPrime(iterator))
+      return iterator;
+
+  return 0;
+}
+
+int main(int argc, char const *argv[]) {
+
+  smallestPrimeDivisorProcessor __smallestPrime__;
+  int number;
+
+  std::cin >> number;
+
+  std::cout << __smallestPrime__.getTheSmallestPrimeDivisor (number);
+
+  return 0;
+}
