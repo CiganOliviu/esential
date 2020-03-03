f1e506fab6a5b1a307b832bd80ee10923ce6b6e8
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-03-03 20:25:38 +0200

    update templates.py

--- easyPass-WS/misha/dependencies/templates.py
+++ easyPass-WS/misha/dependencies/templates.py
@@ -5,14 +5,23 @@ class init_templates():
     def __init__(self):
         super(init_templates, self).__init__()
 
-    def standard_structure_algo_data_structures_templates(self, file_object):
+    def standard_structure_cpp_algo_data_structures_templates(self, file_object):
 
         file_object.write("#include \"dataStructures.hpp\" \n\n")
         file_object.write("int main(int argc, char const *argv[]) {\n\n")
+        file_object.write("\tOneDimensionalArrayType<int> ODAObject;\n")
+        file_object.write("\tmatrixType<int> MTObject;\n")
+        file_object.write("\tbinaryTreeType<int> binaryTreeObject;\n\n")
+        file_object.write("\treturn 0;\n")
+        file_object.write("}")
+
+    def standard_structure_c_algo_data_structures_templates(self, file_object):
+
+        file_object.write("#include \"dataStructures.h\" \n\n")
+        file_object.write("void main(void) {\n\n")
         file_object.write("\tOneDimensionalArrayType ODAObject;\n")
         file_object.write("\tmatrixType MTObject;\n")
         file_object.write("\tbinaryTreeType binaryTreeObject;\n\n")
-        file_object.write("\treturn 0;\n")
         file_object.write("}")
 
     def standard_structure_easyPass_templates(self, file_object):
@@ -56,11 +65,19 @@ class templates(init_templates):
     def __init__(self):
         super(templates, self).__init__()
 
-    def standard_algo_data_structures_templates(self, file_name):
+    def standard_cpp_algo_data_structures_templates(self, file_name):
 
         ADS_template = open(file_name, "w")
 
-        self.standard_structure_algo_data_structures_templates(ADS_template)
+        self.standard_structure_cpp_algo_data_structures_templates(ADS_template)
+
+        ADS_template.close()
+
+    def standard_c_algo_data_structures_templates(self, file_name):
+
+        ADS_template = open(file_name, "w")
+
+        self.standard_structure_c_algo_data_structures_templates(ADS_template)
 
         ADS_template.close()
 
@@ -89,3 +106,8 @@ class templates(init_templates):
         self.standard_misha_script(MISHA_template)
 
         MISHA_template.close()
+
+
+tp = templates()
+
+tp.standard_c_algo_data_structures_templates('main.c')
