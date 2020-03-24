c1866d832772454526547bbcf4788f691d504d55
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-03-24 21:23:49 +0200

    Update dataStructures.hpp

--- easyPass-WS/dependencies/dataStructures.hpp
+++ easyPass-WS/dependencies/dataStructures.hpp
@@ -143,4 +143,15 @@ namespace dataStructures {
 
     virtual ~complexNumber () {}
   };
+
+  template <class Type> class centrum {
+
+  public:
+    centrum () {}
+
+    Type data;
+    centrum<Type> * nextLinkedValue;
+
+    virtual ~centrum () {}
+  };
 }
