5946a24c47d2e10df2c3327061788cc92faad7c5
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-19 21:39:04 +0300

    Update maximumSumSequence

---
+++ samples/maximumSumSequence/maximumSumSequence.cpp
@@ -0,0 +1,35 @@
+#include "../../esential.hpp"
+
+int minimumSequenceSum (oneDimensionalArrayType<int> ODA) {
+
+	errorsHandler __handler__;
+	int sum = 0;
+
+	__handler__.standardHandlerOneDimensionalArray (ODA, __PRETTY_FUNCTION__);
+
+	for (int iterator = ODA.startPoint; iterator < ODA.length + ODA.endPoint; iterator++) {
+		
+		sum += ODA.oneDimensionalArray[iterator];
+
+		if (sum <= 0)
+			sum = 0;	
+	}
+
+	return sum;
+}
+
+int main (int argc, char const * argv[]) {
+
+	IOSystemOneDimensionalArray inputOutputSystem;
+	oneDimensionalArrayType<int> Data;
+
+	inputOutputSystem.readDynamicFileOneDimensionalArray ((char*)"ODA.data", Data);
+
+	int result;
+
+	result = minimumSequenceSum (Data);
+
+	std::cout << result;
+
+	return 0;
+}
